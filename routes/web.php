f83889dd57bf042505007ec80f9399b18b828d6f
Bertho <berthojoris@gmail.com>
2021-01-16 02:32:04 +0700

    Login page use template done

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    if (empty(auth()->user())) {
+        return redirect('/login');
+    } else {
+        return redirect('/home');
+    }
 });
 
 Auth::routes();
