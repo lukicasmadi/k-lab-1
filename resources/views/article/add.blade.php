139cccdb711b96ad5f71ea9bff287392c8100574
Bertho <berthojoris@gmail.com>
2021-01-21 16:18:48 +0700

    Update layout article

--- resources/views/article/add.blade.php
+++ resources/views/article/add.blade.php
@@ -4,7 +4,7 @@
 <div class="layout-px-spacing">
     <div class="row layout-top-spacing">
 
-        <div class="col-lg-12 col-12  layout-spacing">
+        <div class="col-lg-6 col-12  layout-spacing">
             <div class="statbox widget box box-shadow">
 
                 @if ($errors->any())
@@ -19,7 +19,7 @@
 
                 <div class="widget-header">
                     <div class="row">
-                        <div class="col-xl-12 col-md-12 col-sm-12 col-12">
+                        <div class="col-xl-6 col-md-12 col-sm-12 col-12">
                             <h4>Input Data Article</h4>
                         </div>
                     </div>
