d17846ee7a00d6b70026b2baa2e2a966b137c0eb
Bertho <berthojoris@gmail.com>
2021-01-22 20:11:29 +0700

    Update menu id tag

--- resources/views/include/top_bar.blade.php
+++ resources/views/include/top_bar.blade.php
@@ -14,7 +14,7 @@
 
         <ul class="list-unstyled menu-categories" id="topAccordion">
 
-            <li class="menu single-menu {{ request()->is('/') ? 'active' : '' }}">
+            <li id="menuDashboard" class="menu single-menu {{ request()->is('/') ? 'active' : '' }}">
                 <a href="{{ route('dashboard') }}">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
@@ -23,7 +23,7 @@
                 </a>
             </li>
 
-            <li class="menu single-menu {{ request()->is('admin/category') || request()->is('admin/article') || request()->is('admin/polda') ? 'active' : '' }}">
+            <li id="menuMaster" class="menu single-menu {{ request()->is('admin/category') || request()->is('admin/article') || request()->is('admin/polda') ? 'active' : '' }}">
                 <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clipboard"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg>
@@ -31,7 +31,7 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#topAccordion">
+                <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#menuMaster">
                     <li class="{{ request()->is('admin/category/*') || request()->is('admin/category') ? 'active' : '' }}">
                         <a href="{{ route('category_index') }}"> Category </a>
                     </li>
@@ -44,7 +44,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
+            <li id="menuOperation" class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
                 <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-settings"><circle cx="12" cy="12" r="3"></circle><path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path></svg>
@@ -52,7 +52,7 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" data-parent="#topAccordion">
+                <ul class="collapse submenu list-unstyled" data-parent="#menuOperation">
                     <li class="{{ request()->is('admin/rencana-operasi') ? 'active' : '' }}">
                         <a href="{{ route('rencana_operasi_index') }}"> List and Create Operation </a>
                     </li>
@@ -62,7 +62,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
+            <li id="menuReportPusat" class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
                 <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-printer"><polyline points="6 9 6 2 18 2 18 9"></polyline><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path><rect x="6" y="14" width="12" height="8"></rect></svg>
@@ -70,7 +70,7 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" data-parent="#topAccordion">
+                <ul class="collapse submenu list-unstyled" data-parent="#menuReportPusat">
                     <li class="{{ request()->is('admin/rencana-operasi') ? 'active' : '' }}">
                         <a href="{{ route('rencana_operasi_index') }}"> This Week </a>
                     </li>
@@ -98,7 +98,7 @@
                 </ul>
             </li>
 
-            <li class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
+            <li id="menuPolda" class="menu single-menu {{ request()->is('admin/operasi/*') ? 'active' : '' }}">
                 <a href="#" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
                     <div class="">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-printer"><polyline points="6 9 6 2 18 2 18 9"></polyline><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"></path><rect x="6" y="14" width="12" height="8"></rect></svg>
@@ -106,17 +106,13 @@
                     </div>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </a>
-                <ul class="collapse submenu list-unstyled" data-parent="#topAccordion">
+                <ul class="collapse submenu list-unstyled" data-parent="#menuPolda">
                     <li class="{{ request()->is('admin/rencana-operasi') ? 'active' : '' }}">
                         <a href="{{ route('rencana_operasi_index') }}"> By Date </a>
                     </li>
-                </ul>
-                <ul class="collapse submenu list-unstyled" data-parent="#topAccordion">
                     <li class="{{ request()->is('admin/rencana-operasi') ? 'active' : '' }}">
                         <a href="{{ route('rencana_operasi_index') }}"> By Date Range </a>
                     </li>
-                </ul>
-                <ul class="collapse submenu list-unstyled" data-parent="#topAccordion">
                     <li class="{{ request()->is('admin/rencana-operasi') ? 'active' : '' }}">
                         <a href="{{ route('rencana_operasi_index') }}"> By Month </a>
                     </li>
