55d81da8a7638eb287776c6f329b0822adab9fa8
Bertho <berthojoris@gmail.com>
2021-02-16 14:49:03 +0700

    Update logic HomeController for Polda

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -42,11 +42,9 @@ class HomeController extends Controller
 
     public function dailycheck()
     {
-        $model = Polda::with('dailyInput')->orderBy("name", "asc");
-
-        // $model = Polda::whereHas('dailyInput', function($q){
-        //     $q->where(DB::raw('DATE(created_at)'), date("Y-m-d"));
-        // })->orderBy("name", "asc");
+        $model = Polda::with(['dailyInput' => function($query) {
+            $query->where(DB::raw('DATE(created_at)'), date("Y-m-d"));
+        }])->orderBy("name", "asc");
 
         return datatables()->eloquent($model)
         ->addColumn('has_submited', function (Polda $polda) {
@@ -105,10 +103,5 @@ class HomeController extends Controller
             return view('empty_project');
         }
         return view('main', compact('polda'));
-    }
-
-    public function previewReport($uuid)
-    {
-        //
     }
 }
