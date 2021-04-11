210c2021d04ce5063fdf7289a0e3a3838d71ab2a
Bertho <berthojoris@gmail.com>
2021-04-12 02:02:34 +0700

    Update UI

--- resources/views/user/profile_update.blade.php
+++ resources/views/user/profile_update.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.template_admin')
 
+@push('page_title')
+<div class="page-title">
+    <h3>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15.556" viewBox="0 0 20 15.556">
+        <path id="text_align_left" d="M16.333,20.556H3V18.333H16.333ZM23,16.111H3V13.889H23Zm-6.667-4.444H3V9.444H16.333ZM23,7.222H3V5H23Z" transform="translate(-3 -5)" fill="#00adef"/>
+        </svg>
+        <span>UBAH PROFIL</span>
+    </h3>
+</div>
+@endpush
+
 @section('content')
 <div class="layout-px-spacing">
     <div class="row layout-top-spacing">
@@ -7,14 +18,6 @@
         <div class="col-lg-6 col-12  layout-spacing">
             @include('flash::message')
             <div class="statbox widget box box-shadow">
-
-                <div class="widget-header">
-                    <div class="row">
-                        <div class="col-xl-6 col-md-12 col-sm-12 col-12">
-                            <h4>Update Profile</h4>
-                        </div>
-                    </div>
-                </div>
 
                 <div class="widget-content widget-content-area">
                     <form method="POST" action="{{ route('profile_process') }}" enctype="multipart/form-data">
