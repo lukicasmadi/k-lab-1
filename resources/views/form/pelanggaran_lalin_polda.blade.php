0f87e3c34fed49f6d8b5e413f37f367edb1928b2
Bertho <berthojoris@gmail.com>
2021-04-28 14:10:54 +0700

    Adjust layout

--- resources/views/form/pelanggaran_lalin_polda.blade.php
+++ resources/views/form/pelanggaran_lalin_polda.blade.php
@@ -82,7 +82,7 @@
                 @enderror
             </div>
             <div class="col-md-4 mb-4">
-                <label class="text-popup">Upload Dokomen (pdf, ppt, zip)</label>
+                <label class="text-popup">Upload Dokomen (pdf, ppt, word, zip)</label>
                 <input type="file" class="form-control popoups mt-1 @error('document_upload') is-invalid @enderror" name="document_upload" id="document_upload">
                 @error('document_upload')
                     <span class="invalid-feedback" role="alert">
