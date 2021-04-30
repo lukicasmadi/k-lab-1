546d71d6f0fbc58e9689ea6355a8565fbf2d8ddb
Bertho <berthojoris@gmail.com>
2021-04-30 13:43:04 +0700

    Korlantas dashboard view polda data using popup window

--- app/Http/Controllers/PoldaHasRencanaOperasiController.php
+++ app/Http/Controllers/PoldaHasRencanaOperasiController.php
@@ -893,35 +893,19 @@ class PoldaHasRencanaOperasiController extends Controller
 
     public function previewPhroDashboard(Request $request, $uuid)
     {
-        if (! $request->ajax()) {
-            abort(401);
-        }
-
         $polda = Polda::whereUuid($uuid)->firstOrFail();
-        $data = PoldaSubmited::where("polda_id", $polda->id)->where('submited_date', date('Y-m-d'))->firstOrFail();
-        $daily = DailyInput::select('year')->where("polda_submited_id", $data->id)->first();
-        $dailyPrev = DailyInputPrev::select('year')->where("polda_submited_id", $data->id)->first();
 
-        $dailyInput = dailyInput(
-            $data->rencana_operasi_id,
-            date('Y'),
-            $data->polda_id,
-            date('Y-m-d')
+        $poldaSubmited = PoldaSubmited::where("polda_id", $polda->id)->where('submited_date', nowToday())->firstOrFail();
+
+        $prev = reportDailyPrev($polda->id, yearMinus(), $poldaSubmited->rencana_operasi_id, null, nowToday(), nowToday());
+        $current = reportDailyCurrent($polda->id, year(), $poldaSubmited->rencana_operasi_id, null, nowToday(), nowToday());
+
+        return excelTemplateDisplay(
+            $prev,
+            $current,
+            yearMinus(),
+            year()
         );
-
-        $dailyInputPrev = dailyInputPrev(
-            $data->rencana_operasi_id,
-            date('Y'),
-            $data->polda_id,
-            date('Y-m-d')
-        );
-
-        return [
-            'dailyInput' => $dailyInput,
-            'dailyInputPrev' => $dailyInputPrev,
-            'daily' => $daily,
-            'dailyPrev' => $dailyPrev,
-        ];
     }
 
     public function download($uuid)
