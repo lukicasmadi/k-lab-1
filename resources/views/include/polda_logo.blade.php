8cd414c4f8c0938bf53d1a32eb65c76228eadb4a
lukicasmadi <75202376+lukicasmadi@users.noreply.github.com>
2021-05-09 00:46:05 +0700

    polda 34 logo

--- resources/views/include/polda_logo.blade.php
+++ resources/views/include/polda_logo.blade.php
@@ -11,10 +11,10 @@
     @endforeach
 </div>
 
-<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing mt-1 mb-n2">
+<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
     @foreach ($poldaBawah as $key => $val)
         <a href="{{ route('korlantas_open_polda', $val->short_name) }}">
-            <div class="cols-sm-1">
+            <div class="cols-sm-1 mt-1">
                 <div class="grid-polda line @if (empty($val->dailyInput)) glowred @else glowblue @endif">
                     <p>{{ $val->short_name }}</p>
                     <img src="{{ asset('/img/polda/'.$val->logo) }}">
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -21,8 +21,6 @@
             <img src="{{ asset('/img/line-poldaup.png') }}" width="100%">
         </div>
 
-        
-
         <div class="col-xl-6 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
             <div class="">
                 <div class="widget-heading">
