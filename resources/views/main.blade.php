293cd64b4a0583d41c8fdfc4184f1261d7ea7c8d
Bertho <berthojoris@gmail.com>
2021-05-20 18:00:57 +0700

    Created tabs done

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -92,95 +92,10 @@
         @role('access_pusat|administrator')
             @if (!empty(operationPlans()))
                 @if (authUser()->id == 1)
-                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                        <div class="widget-content justify-pill">
-                            <ul class="nav nav-pills2 nav-fill" id="justify-pills-tab" role="tablist">
-                                <li class="nav-item mr-3">
-                                    <a class="nav-link2 active" id="justify-pills-home-tab" data-toggle="pill" href="#justify-pills-home" role="tab" aria-controls="justify-pills-home" aria-selected="true">Data Absensi Polda</a>
-                                </li>
-                                <li class="nav-item mr-3">
-                                    <a class="nav-link2" id="justify-pills-profile-tab" data-toggle="pill" href="#justify-pills-profile" role="tab" aria-controls="justify-pills-profile" aria-selected="false">Data Laporan Harian</a>
-                                </li>
-                                <li class="nav-item mr-3">
-                                    <a class="nav-link2" id="justify-pills-contact-tab" data-toggle="pill" href="#justify-pills-contact" role="tab" aria-controls="justify-pills-contact" aria-selected="false">Data Total Laporan Harian</a>
-                                </li>
-                                <li class="nav-item mr-3">
-                                    <a class="nav-link2" id="justify-pills-contact-tab" data-toggle="pill" href="#justify-pills-contact" role="tab" aria-controls="justify-pills-contact" aria-selected="false">Data Laporan Anev</a>
-                                </li>
-                                <li class="nav-item mr-3">
-                                    <a class="nav-link2" id="justify-pills-contact-tab" data-toggle="pill" href="#justify-pills-contact" role="tab" aria-controls="justify-pills-contact" aria-selected="false">Data Total Laporan Anev</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link2" id="justify-pills-contact-tab" data-toggle="pill" href="#justify-pills-contact" role="tab" aria-controls="justify-pills-contact" aria-selected="false">Semua Data Laporan</a>
-                                </li>
-                            </ul>
-
-                            <div class="tab-content" id="justify-pills-tabContent">
-                                <div class="tab-pane fade show active" id="justify-pills-home" role="tabpanel" aria-labelledby="justify-pills-home-tab">
-                                    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                                        <div class="widget widget-activity-three">
-                                            <div class="widget-heading">
-                                                <div class="row">
-                                                    <h5 class="ml-n3">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" class="ml-n1" width="20" height="20" viewBox="0 0 20 20">
-                                                            <path
-                                                                id="bar_chart_alt"
-                                                                d="M22,22H2V11.474A2.055,2.055,0,0,1,4,9.368H8V4.105A2.055,2.055,0,0,1,10,2h4a2.055,2.055,0,0,1,2,2.105V7.263h4a2.055,2.055,0,0,1,2,2.105ZM16,9.368V19.895h4V9.368ZM10,4.105V19.895h4V4.105ZM4,11.474v8.421H8V11.474Z"
-                                                                transform="translate(-2 -2)"
-                                                                fill="#00adef"
-                                                            />
-                                                        </svg>
-                                                        <span>DATA ABSENSI POLDA</span>
-                                                        <p class="mar20 ml-n1">total data laporan</p>
-                                                        <b class="ml-n1">34 laporan</b>
-                                                    </h5>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="tab-pane fade" id="justify-pills-profile" role="tabpanel" aria-labelledby="justify-pills-profile-tab">
-                                    <div class="media"></div>
-                                </div>
-
-                                <div class="tab-pane fade" id="justify-pills-contact" role="tabpanel" aria-labelledby="justify-pills-contact-tab"></div>
-                            </div>
-                        </div>
-                    </div>
+                    @include('home_tabs.tab')
                 @endif
             @endif
         @endrole
-
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-            <div class="widget widget-activity-three">
-                <div class="widget-heading">
-                    <div class="row">
-                        <h5 class="ml-n3">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="ml-n1" width="20" height="20" viewBox="0 0 20 20">
-                                <path id="bar_chart_alt" d="M22,22H2V11.474A2.055,2.055,0,0,1,4,9.368H8V4.105A2.055,2.055,0,0,1,10,2h4a2.055,2.055,0,0,1,2,2.105V7.263h4a2.055,2.055,0,0,1,2,2.105ZM16,9.368V19.895h4V9.368ZM10,4.105V19.895h4V4.105ZM4,11.474v8.421H8V11.474Z" transform="translate(-2 -2)" fill="#00adef"/>
-                            </svg>
-                            <span>DATA ABSENSI POLDA</span>
-                            <p class="mar20 ml-n1">total data laporan</p>
-                            <b class="ml-n1">34 laporan</b>
-                        </h5>
-                    </div>
-                </div>
-                <div class="widget-content">
-                    <div class="row">
-                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing">
-                            <div class="mx-auto">
-                                <div id="incoming_report"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 layout-spacing mt-n3">
-            <img src="{{ asset('/img/line-poldaup.png') }}" width="100%">
-        </div>
 
         <div class="col-xl-12 col-lg-12 col-sm-12  layout-spacing">
             <div class="widget-heading mt-2">
@@ -269,9 +184,7 @@
 @endpush
 
 @push('library_css')
-<link rel="stylesheet" type="text/css" href="{{ asset('template/plugins/table/datatable/datatables.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('template/plugins/table/datatable/dt-global_style.css') }}" />
-<!-- <link rel="stylesheet" type="text/css" href="{{ asset('template/plugins/dist-apex/apexcharts.css') }}" /> -->
+<link rel="stylesheet" type="text/css" href="{{ asset('template/plugins/dist-apex/apexcharts.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/dashboard/dash_2.css') }}" />
 <link rel="stylesheet" type="text/css" href="{{ asset('template/plugins/animate/animate.css') }}"/>
 <link rel="stylesheet" type="text/css" href="{{ asset('template/assets/css/components/custom-modal.css') }}"/>
