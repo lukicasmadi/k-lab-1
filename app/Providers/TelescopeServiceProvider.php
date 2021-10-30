d393c9cdcdd78b8fbe72d1b163bdcdcea4388636
Bertho <berthojoris@gmail.com>
2021-10-30 22:58:09 +0700

    Update

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Str;
+use Laravel\Telescope\EntryType;
+use Laravel\Telescope\Telescope;
 use Illuminate\Support\Facades\Gate;
 use Laravel\Telescope\IncomingEntry;
-use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
@@ -19,6 +21,45 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         // Telescope::night();
 
         $this->hideSensitiveRequestDetails();
+
+        // Telescope::tag(function (IncomingEntry $entry) {
+
+        //     if ($entry->type === EntryType::EXCEPTION) {
+        //         $entry->tags([
+        //             $entry->content['class'],
+        //         ]);
+        //     }
+
+        //     if ($entry->type === EntryType::MAIL) {
+        //         $entry->tags([
+        //             $entry->content['mailable'],
+        //         ]);
+        //     }
+
+        //     if ($entry->type === EntryType::REQUEST) {
+        //         $entry->tags([
+        //             $entry->content['uri'],
+        //             $entry->content['method'],
+        //             $entry->content['response_status'],
+        //             $entry->content['controller_action'],
+        //         ]);
+        //     }
+
+        //     if ($entry->type === EntryType::VIEW) {
+        //         $entry->tags([
+        //             $entry->content['path'],
+        //         ]);
+        //     }
+
+        //     if ($entry->type === EntryType::MODEL) {
+        //         $entry->tags([
+        //             $entry->content['model'],
+        //             $entry->content['action'],
+        //         ]);
+        //     }
+
+        //     return true;
+        // });
 
         Telescope::filter(function (IncomingEntry $entry) {
             // if ($this->app->environment('local')) {
