178190837bd273526866396afdf1a16300a26508
Bertho <berthojoris@gmail.com>
2021-11-10 16:53:31 +0700

    Hide action card

--- resources/views/report/progress.blade.php
+++ resources/views/report/progress.blade.php
@@ -34,7 +34,7 @@
                             <h6>Pending Tasks ID</h6>
                             <p class="meta-date">{{ $batch->id }}</p>
                         </div>
-                        <div class="task-action">
+                        {{-- <div class="task-action">
                             <div class="dropdown  custom-dropdown">
                                 <a class="dropdown-toggle" href="#" role="button" id="pendingTask" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
@@ -47,7 +47,7 @@
                                     <a class="dropdown-item" href="javascript:void(0);">Clear All</a>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                     </div>
 
                     <div class="w-content">
