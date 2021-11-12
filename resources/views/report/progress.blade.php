50bcf183043e8f616ffcd1d1b99c8fada565ef38
Bertho <berthojoris@gmail.com>
2021-11-12 09:35:12 +0700

    Remove unused code

--- resources/views/report/progress.blade.php
+++ resources/views/report/progress.blade.php
@@ -12,17 +12,6 @@
 @section('content')
 <div class="layout-px-spacing">
     <div class="row layout-top-spacing">
-        {{--
-        <div class="col-lg-12 col-12">
-            <div class="statbox widget box box-shadow">
-                <div class="widget-content">
-                    <p><h3><span id="processJob">{{ $batch->processedJobs() }}</span> selesai dari total <span id="totalJobs">{{ $batch->totalJobs }}</span> data yg diproses</h3></p>
-                    <p><h3>Proses <span id="progress">{{ $batch->progress() }}</span>%</h3></p>
-                </div>
-                <a href="{{ route('open_ready_report', session("progres_report_id")) }}" id="visitBtn" class="btn btn-info invisible">View Report</a>
-            </div>
-        </div> --}}
-
         <input type="hidden" name="batch_id" id="batch_id" value="{{ $batch->id }}">
 
         <div class="col-xl-4 col-lg-6 col-md-6 col-sm-6 col-12 layout-spacing">
@@ -32,22 +21,8 @@
                     <div class="header">
                         <div class="header-body">
                             <h6>Processing Tasks {{ $batch->name }}</h6>
-                            <p class="meta-date">{{ $batch->id }}</p>
+                            <p class="meta-date">ID Task : {{ $batch->id }}</p>
                         </div>
-                        {{-- <div class="task-action">
-                            <div class="dropdown  custom-dropdown">
-                                <a class="dropdown-toggle" href="#" role="button" id="pendingTask" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-horizontal"><circle cx="12" cy="12" r="1"></circle><circle cx="19" cy="12" r="1"></circle><circle cx="5" cy="12" r="1"></circle></svg>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="pendingTask">
-                                    <a class="dropdown-item" href="javascript:void(0);">Add</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">View</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Update</a>
-                                    <a class="dropdown-item" href="javascript:void(0);">Clear All</a>
-                                </div>
-                            </div>
-                        </div> --}}
                     </div>
 
                     <div class="w-content">
