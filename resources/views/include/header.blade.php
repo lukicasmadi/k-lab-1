01ed61e238a5e84b81535e766944a542e45588b8
Bertho <berthojoris@gmail.com>
2021-11-14 00:36:31 +0700

    Show menu report operasi

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -72,11 +72,9 @@
                                     <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
                                         <a href="{{ route('report_all_polda_compare') }}"> Laporan Perbandingan Harian </a>
                                     </li>
-                                    @if (myUserId() == 1)
-                                        <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
-                                            <a href="#" id="report_polda_all"> Laporan Operasi </a>
-                                        </li>
-                                    @endif
+                                    <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
+                                        <a href="#" id="report_polda_all"> Laporan Operasi </a>
+                                    </li>
                                 </ul>
                             </li>
                         @endrole
@@ -343,11 +341,9 @@
                                     <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
                                         <a href="{{ route('report_all_polda_compare') }}"> Laporan Perbandingan Harian </a>
                                     </li>
-                                    @if (myUserId() == 1)
-                                        <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
-                                            <a href="#" id="report_polda_all"> Laporan Operasi </a>
-                                        </li>
-                                    @endif
+                                    <li class="{{ request()->is('report/polda/all/daily-compare') ? 'active' : '' }}">
+                                        <a href="#" id="report_polda_all"> Laporan Operasi </a>
+                                    </li>
                                 </ul>
                             </li>
                         @endrole
