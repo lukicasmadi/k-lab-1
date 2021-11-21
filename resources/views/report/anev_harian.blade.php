46544a787e96e8f3942f382c5591596057f21ae2
Bertho <berthojoris@gmail.com>
2021-11-21 21:42:19 +0700

    Fix laporan anev harian

--- resources/views/report/anev_harian.blade.php
+++ resources/views/report/anev_harian.blade.php
@@ -5,7 +5,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="15.556" viewBox="0 0 20 15.556">
             <path id="text_align_left" d="M16.333,20.556H3V18.333H16.333ZM23,16.111H3V13.889H23Zm-6.667-4.444H3V9.444H16.333ZM23,7.222H3V5H23Z" transform="translate(-3 -5)" fill="#00adef"/>
         </svg>
-        <span>LAPORAN ANEV HARIAN</span>
+        <span>LAPORAN ANALISA & EVALUASI HARIAN</span>
     </h3>
 </div>
 @endpush
