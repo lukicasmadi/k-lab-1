cc0a8085beb702124b6e7201ebdc710d3be30d5a
Bertho <berthojoris@gmail.com>
2021-11-23 19:47:56 +0700

    Fix wording

--- resources/views/home_tabs/tab_absensi.blade.php
+++ resources/views/home_tabs/tab_absensi.blade.php
@@ -6,7 +6,7 @@
                     <svg xmlns="http://www.w3.org/2000/svg" class="ml-n1" width="20" height="20" viewBox="0 0 20 20">
                         <path id="bar_chart_alt" d="M22,22H2V11.474A2.055,2.055,0,0,1,4,9.368H8V4.105A2.055,2.055,0,0,1,10,2h4a2.055,2.055,0,0,1,2,2.105V7.263h4a2.055,2.055,0,0,1,2,2.105ZM16,9.368V19.895h4V9.368ZM10,4.105V19.895h4V4.105ZM4,11.474v8.421H8V11.474Z" transform="translate(-2 -2)" fill="#00adef"/>
                     </svg>
-                    <span>DATA ABSENSI POLDA</span>
+                    <span>DATA ABSENSI LAPORAN HARIAN POLDA</span>
                     <p class="mar20 ml-n1">total data laporan</p>
                     <b class="ml-n1">34 laporan</b>
                 </h5>
